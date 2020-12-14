4502a463ab54e995a5a7d1909cc3620831cb6bc1
martinRenou <martin.renou@gmail.com>
2020-12-14 12:01:21 +0100

    Release 0.0.6
    Signed-off-by: martinRenou <martin.renou@gmail.com>

--- src/xeus_robot_config.hpp
+++ src/xeus_robot_config.hpp
@@ -14,7 +14,7 @@
 // Project version
 #define XROB_VERSION_MAJOR 0
 #define XROB_VERSION_MINOR 0
-#define XROB_VERSION_PATCH 5
+#define XROB_VERSION_PATCH 6
 
 // Composing the version string from major, minor and patch
 #define XROB_CONCATENATE(A, B) XROB_CONCATENATE_IMPL(A, B)
