989e4bfb9ce01b7a3c8f9cc7224a212de7f1a9e3
martinRenou <martin.renou@gmail.com>
2021-02-19 09:16:47 +0100

    Release 0.3.0

--- src/xeus_robot_config.hpp
+++ src/xeus_robot_config.hpp
@@ -13,8 +13,8 @@
 
 // Project version
 #define XROB_VERSION_MAJOR 0
-#define XROB_VERSION_MINOR 2
-#define XROB_VERSION_PATCH 2
+#define XROB_VERSION_MINOR 3
+#define XROB_VERSION_PATCH 0
 
 // Composing the version string from major, minor and patch
 #define XROB_CONCATENATE(A, B) XROB_CONCATENATE_IMPL(A, B)
