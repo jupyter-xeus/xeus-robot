7464c1352d00d20718cbbd31e2826aa3f433b2ea
Johan Mabille <johan.mabille@gmail.com>
2021-09-20 09:57:52 +0200

    Release 0.4.0

--- src/xeus_robot_config.hpp
+++ src/xeus_robot_config.hpp
@@ -13,8 +13,8 @@
 
 // Project version
 #define XROB_VERSION_MAJOR 0
-#define XROB_VERSION_MINOR 3
-#define XROB_VERSION_PATCH 8
+#define XROB_VERSION_MINOR 4
+#define XROB_VERSION_PATCH 0
 
 // Composing the version string from major, minor and patch
 #define XROB_CONCATENATE(A, B) XROB_CONCATENATE_IMPL(A, B)
