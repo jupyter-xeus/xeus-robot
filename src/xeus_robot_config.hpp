5513ee31c78b6c306d72202c62926c022427c73d
Sylvain Corlay <sylvain.corlay@gmail.com>
2023-01-10 22:16:52 +0100

    Release 0.5.0

--- src/xeus_robot_config.hpp
+++ src/xeus_robot_config.hpp
@@ -13,8 +13,8 @@
 
 // Project version
 #define XROB_VERSION_MAJOR 0
-#define XROB_VERSION_MINOR 4
-#define XROB_VERSION_PATCH 4
+#define XROB_VERSION_MINOR 5
+#define XROB_VERSION_PATCH 0
 
 // Composing the version string from major, minor and patch
 #define XROB_CONCATENATE(A, B) XROB_CONCATENATE_IMPL(A, B)
